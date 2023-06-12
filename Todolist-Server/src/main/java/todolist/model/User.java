e0907eb18a2597db264f9594c01d761f16f1c60c
Hawgnes <30411458+Hawgnes@users.noreply.github.com>
2023-06-12 21:09:23 +0800

    implemented User's CRUD

--- Todolist-Server/src/main/java/todolist/model/User.java
+++ Todolist-Server/src/main/java/todolist/model/User.java
@@ -7,29 +7,38 @@ import java.util.List;
 import java.util.UUID;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.domain.Persistable;
 import org.springframework.data.relational.core.mapping.Table;
 
 import lombok.Data;
 
 @Data
 @Table
-public class User {
+public class User implements Persistable<Long>{
 	@Id
 	private Long userId;
 	
 	private String email;
 	private String password;
-	private String apiKey;
+	private String apiKey = UUID.randomUUID().toString();
 	
-	private Date createdAt;
+	private Date createdAt = new Date();
 
+	/*
 	private List<Todo> todoList = new ArrayList<>();
-	
-	void createdAt() {
-		this.createdAt = new Date();
-	}
 	
 	public void addTodo(Todo todo) {
 		this.todoList.add(todo);
+	}
+	 */
+	@Override
+	public Long getId() {
+		return this.userId;
+	}
+
+	@Override
+	public boolean isNew() {
+		// mark as true if no userId
+		return this.userId == null;
 	}
 }
